7010cdcba4a84664a0d84d87663e82eac3e6c1f1
Daniel Moffat <danmofo@gmail.com>
2016-03-29 15:39:39 +0100

    Sorted out the nightmare that is Java logging.
    - Spring logs should now appear alongside application logs.
    - Now uses Log4j2
    
    Some useful links that helped me in this idiotic journey:
    - **The winner!** - http://stackoverflow.com/questions/22270755/how-to-redirect-all-logs-from-hibernate-and-spring-to-log4j2/27399414
    - http://docs.spring.io/spring/docs/current/spring-framework-reference/htmlsingle/#overview-logging
    - http://logging.apache.org/log4j/2.x/manual/layouts.html
    - http://logging.apache.org/log4j/2.x/manual/migration.html
    - http://logging.apache.org/log4j/1.2/faq.html

--- src/com/daniel/spring/web/controller/HomeController.java
+++ src/com/daniel/spring/web/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.daniel.spring.web.controller;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
